c1b3dd1673b03c3e0060ca47948ce5024985c6b4
Omidiji Peter Busayo <omidijipeter64@gmail.com>
2022-04-12 10:05:43 +0100

    My first commit - Module 1 & 2

---
+++ color picker/script.js
@@ -0,0 +1,7 @@
+let colorIndicator = document.getElementById('color-indicator');
+const colorPicker = new iro.ColorPicker("#color-picker",{
+    width:180, color: "#fff"
+});
+colorPicker.on('color:change', function(color){
+    colorIndicator.style.backgroundColor = color.hexString;
+});
---
+++ sign up/script.js
@@ -0,0 +1,15 @@
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username =="admin" || password=="admin"){
+        alert("Login successful");
+        return true;
+    } else{
+        alert("Login failed");
+        return false;
+
+
+    }
+}
+
